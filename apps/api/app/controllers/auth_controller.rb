64f1e9df7106db359a71294523e740c71f04ed58
megmingstory-vivy <megmingstory@yahoo.co.jp>
2026-01-21 16:12:28 +0900

    認証周りの土台完成。次はログインとJWT発行

---
+++ apps/api/app/controllers/auth_controller.rb
@@ -0,0 +1,18 @@
+class AuthController < ApplicationController
+ def signup
+    user = User.new(user_params)
+
+    if user.save
+      render json: { message: 'User created' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+   params.require(:user).permit(:email, :password)
+  end
+
+end
--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post "auth/signup", to: "auth#signup"
+  post "auth/login",  to: "auth#login"
+
   get "quizzes/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
