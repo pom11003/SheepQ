da257c41cd13609cf60ee81126b9339531a134b5
pompom <pom@pompomnoMacBook-Air.local>
2026-01-21 12:48:00 +0900

    api/public/images/quizzes クイズ用写真置き場を作成

--- apps/api/app/controllers/quizzes_controller.rb
+++ apps/api/app/controllers/quizzes_controller.rb
@@ -2,10 +2,11 @@
 class QuizzesController < ApplicationController
   def index
     quizzes = Quiz
-      .where(is_published: true)
-      .includes(:choices)
-      .order(created_at: :desc)
+      .where(is_published: true) # 公開中のクイズだけ取り出す
+      .includes(:choices)# そのクイズに紐づく「4択」も一緒に取り出す
+      .order(created_at: :desc) # 新しい順に並べたい
 
-    render json: quizzes.as_json(include: { choices: { only: [:id, :text, :sort_order] } })
+      # JSONで「問題文＋4択」までまとめて返す。ただしchoices は id, text, sort_order だけを含める。
+    render json: quizzes.as_json(include: { choices: { only: [:id, :text, :sort_order] } }) 
   end
 end
